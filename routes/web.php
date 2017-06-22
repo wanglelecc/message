4e7cbd0e37cf4d242bc89e9bdd9fb9ef8ef95001
wanglelecc <wanglelecc@gmail.com>
2017-06-22 23:07:12 +0800

    初始化用户登录系统

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::auth();
+Route::get('/', 'HomeController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
